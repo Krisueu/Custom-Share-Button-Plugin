fa8886628d69bbe94d372bde97f51430a8fec6f6
KRISÚ <157402956+K-risu@users.noreply.github.com>
2024-02-12 23:05:55 +0100

    Update custom-share-button.php
    Version 1.1

--- custom-share-button.php
+++ custom-share-button.php
@@ -1,9 +1,11 @@
 <?php
 /*
 Plugin Name: Custom Share Button
-Description: Ein einfaches Plugin, das es ermöglicht, Beiträge über einen benutzerdefinierten Share-Button zu teilen.
-Version: 1.0
-Author: <a href="https://krisu.eu">Krisu<a>
+Description: A simple plugin that allows sharing posts via a custom share button.
+Version: 1.1
+Author: Krisu
+Author URI: <a href="https://krisu.eu">Krisu<a>
+Icon: images/CustomShareButtonPlugin-Krisu.png
 */
 
 // Shortcode hinzufügen für den Share-Button
